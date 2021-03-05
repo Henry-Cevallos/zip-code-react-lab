da40f3696f7552083f481d7727da1d6ef75241d9
Henry Cevallos <henry.cevallos27@gmail.com>
2021-03-05 16:30:12 -0500

    added basic imp

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,27 +1,68 @@
 import React, { Component } from 'react';
 import './App.css';
-
-
-function City(props) {
-  return (<div>This is the City component</div>);
+function City({city, state}) {
+  return (
+    <div>
+      This is the City component for {city}, {state}
+    </div>
+  );
 }
-
-function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+function ZipSearchField({ onZipChange }) {
+  return (
+    <div>
+      <label>Zip Code:</label>
+      <input type="text" onChange={onZipChange} />
+    </div>
+  );
 }
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipCode: '',
+      cities: [],
+    }
+  }
+  
+  zipChanged(e) {
+    let zip = e.target.value;
+    this.setState({
+      zipCode: zip
+    })
+
+    // Make GET request for the zip resource
+    // then, when you receive the result, store it in state
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+      .then(res => res.json())
+      .then(jsonData => {
+        console.log(jsonData);
+        this.setState({
+          cities: jsonData
+        })
+      })
+      
+     
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField onZipChange={(e) => this.zipChanged(e)} />
         <div>
-          <City />
-          <City />
+          {/*
+            Instead of hardcoding the following 3 cities,
+            Create them dynamically from this.state.cities
+          */}
+          {
+            this.state.cities.map(c => {
+              return <City city={c.City} state={c.State}/>;
+            })
+          }
         </div>
       </div>
     );
